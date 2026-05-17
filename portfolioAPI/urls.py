e78375a80a233314a06c0ced6affc2bec8a00a27
Sanja Mandic <sanja1mandic@gmail.com>
2026-05-17 09:49:53 +0000

    Changes made on PythonAnywhere

--- portfolioAPI/urls.py
+++ portfolioAPI/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 API_PREFIX = 'api/v1/'
 
@@ -26,3 +28,5 @@ urlpatterns = [
     path(API_PREFIX, include('articles.urls')),
     path(API_PREFIX, include('quotes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
