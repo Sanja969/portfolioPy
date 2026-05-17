748f6b8238dcc2c5279ecf52d82b4d44652f69fc
Sanja Mandic <sanja1mandic@gmail.com>
2026-05-17 11:55:05 +0200

    update order for the projects to show last created first

--- projects/models.py
+++ projects/models.py
@@ -10,5 +10,8 @@ class Project(models.Model):
   created_at = models.DateTimeField(auto_now_add=True) 
   updated_at = models.DateTimeField(auto_now=True)
   
+  class Meta:
+    ordering = ['-created_at']
+  
   def __str__(self):
     return self.name
